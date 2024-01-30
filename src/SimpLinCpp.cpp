c740dd923ba471e38e21e6706281eecf2202b01d
kmiles <kmiles14@umd.edu>
2024-01-30 16:25:00 -0700

    Name list output in SimpLinCpp

--- src/SimpLinCpp.cpp
+++ src/SimpLinCpp.cpp
@@ -11,8 +11,13 @@ List SimpLinCpp(arma::vec x, arma::vec y) {
   colvec pred_vals = xmat*beta;
   colvec resid = y - pred_vals;
   colvec se = sqrt(as_scalar(arma::trans(resid)*resid / (y.size() - 2)) * diagvec(inv(trans(xmat)*xmat)));
-  colvec ci_upper = beta + se*R::qnorm(.975, 0, 1, 1, 0);
-  colvec ci_lower = beta - se*R::qnorm(.975, 0, 1, 1, 0);
+  colvec ci_upper = beta + se*R::qt(.975, x.size() - 1, 1, 0);
+  colvec ci_lower = beta - se*R::qt(.975, x.size() - 1, 1, 0);
   mat ci_mat = join_rows(ci_lower, ci_upper);
-  return List::create(beta, se, ci_upper, ci_lower, resid, pred_vals);
+  return List::create(Named("coefficients") = beta, 
+                      Named("se") = se, 
+                      Named("CI_upper") = ci_upper,
+                      Named("CI_lower") = ci_lower, 
+                      Named("Residuals") = resid, 
+                      Named("Fitted_Values") = pred_vals);
 }
